8dbd023eed8146b86694bdb6a86d1590c3b7dd06
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-21 17:40:02 +0200

    new commit

--- start_git.sh
+++ start_git.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 git --version
-cd MSTxLake
 git pull
 open MSTxLake.logicx
--- stop_git.sh
+++ stop_git.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 git --version
-cd MSTxLake
 git add .
 git commit -m 'new commit'
 git push
