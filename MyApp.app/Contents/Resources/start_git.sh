5aa3696f34aab9b4a3c0fb363e78e0a115752142
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-21 20:09:15 +0200

    new commit

---
+++ MyApp.app/Contents/Resources/start_git.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git --version
+git fetch --all
+git reset --hard origin/main
+open MSTxLake.logicx
---
+++ MyApp.app/Contents/Resources/stop_git.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git --version
+git add --all
+git commit -m 'new commit'
+git push
