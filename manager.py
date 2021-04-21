0366980b74bcc57f5c4684f61e5dee271d168a7d
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-21 19:54:17 +0200

    new commit

--- manager.py
+++ manager.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import os
-import tkinter as tk
+import tkinter
+from tkinter import *
 
 TK_SILENCE_DEPRECATION = 1
 
-window = tk.Tk()
+window = Tk()
 window.title("MST & Lake Suburbia maken muziek")
-canvas = tk.Canvas(window, width = 450, height = 500)
+canvas = Canvas(window, width = 450, height = 500)
 
 frame = tk.Frame(window, bg="")
 frame.place(relx=0.05, rely=0.05, relwidth=0.9, relheight= 0.9)
@@ -20,9 +21,9 @@ def stop():
     os.system("bash stop_git.sh")
 
 
-startButton = tk.Button(frame, text="START", command=start)
+startButton = Button(frame, text="START", command=start)
 startButton.pack()
-stopButton = tk.Button(frame, text="STOP", command=stop)
+stopButton = Button(frame, text="STOP", command=stop)
 stopButton.pack()
 
 window.mainloop() 
